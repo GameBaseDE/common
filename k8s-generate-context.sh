6b8964012f16d10d904d1c0e2cca38c5d71e9371
Kevin Reis <kr-git@outlook.com>
2019-12-05 09:06:08 +0100

    Refactoring methods and add kubectl_token()

--- k8s-generate-context.sh
+++ k8s-generate-context.sh
@@ -8,14 +8,12 @@ print_usage()
 
 kubectl_user()
 {
-    kubectl config set-cluster gamebase-cluster --cluster=https://kubernetes.gahr.dev
     kubectl config set-credentials gamebase-user --user="$2" --password="$3"
-    kubectl config set-context gamebase-context --cluster=gamebase-cluster --user=gamebase-user
 }
 
 kubectl_token()
 {
-    echo "To be implemented."
+    kubectl config set-credentials gamebase-user --token="$2"
 }
 
 if [ $# -lt 1 ] then
@@ -26,10 +24,15 @@ else if [ $# -gt 3 ] then
     print_usage()
 fi
 
+kubectl config set-cluster gamebase-cluster --cluster=https://kubernetes.gahr.dev
 if [ $1 -eq 'user' ] then
     kubectl_user()
-else [ $1 -eq 'token' ] then
+else if [ $1 -eq 'token' ] then
     kubectl_token()
+else
+    echo "ERROR: Your first parameter is invalid."
+    print_usage()
 fi
+kubectl config set-context gamebase-context --cluster=gamebase-cluster --user=gamebase-user
 
 echo "Done. Use 'kubectl config use-context gamebase-cluster' in order to access GameBase K8S cluster."
